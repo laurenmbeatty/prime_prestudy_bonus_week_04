62b7020f1307115d1cdb08f2e426e399ba6cbf1f
Lauren Beatty <lauren.m.beatty@gmail.com>
2015-08-15 18:24:30 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function splinter(num1, num2) {
+	var z = num1 * num2;
+	var myArray = [];
+	myArray.push(num1);
+	myArray.push(num2);
+	myArray.push(z);
+	return myArray;
+}
+
+console.log(splinter(1,2));
+
